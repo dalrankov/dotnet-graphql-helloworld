5666df1f58b0d864c8b267f5dd86f780fd9dfef9
Dalibor Rankovic <dalrankov@icloud.com>
2022-03-02 22:14:13 +0100

    code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>();
+var app = builder.Build();
+app.MapGraphQL();
+app.Run();
+
+public class Query
+{
+    public string Hello => "World!";
+}
